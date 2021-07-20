bf43fdb457771bcc5a52fb3aeb30b7e890bf336c
44050496 <ra.redmer@outlook.com>
2021-07-20 19:15:09 +0200

    Add rng compatible with interface of russian roulette

--- contracts/IRussianRoulette.sol
+++ contracts/IRussianRoulette.sol
@@ -1,22 +1,20 @@
 //SPDX-License-Identifier: MIT
-pragma solidity >= 0.6.0 < 0.8.0;
+pragma solidity >=0.6.0 <0.8.0;
 
 interface IRussianRoulette {
-
     //-------------------------------------------------------------------------
     // VIEW FUNCTIONS
     //-------------------------------------------------------------------------
 
-    function getMaxRange() external view returns(uint16);
+    function getMaxRange() external view returns (uint16);
 
     //-------------------------------------------------------------------------
-    // STATE MODIFYING FUNCTIONS 
+    // STATE MODIFYING FUNCTIONS
     //-------------------------------------------------------------------------
 
-    function numbersDrawn(
-        uint256 _lotteryId,
-        bytes32 _requestId, 
+    function numberDrawn(
+        uint256 _russianRouletteId,
+        bytes32 _requestId,
         uint256 _randomNumber
-    ) 
-        external;
+    ) external;
 }
