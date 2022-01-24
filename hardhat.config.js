b82f135915dbb339c09ba02466906ee49c452541
eve-contract-person <tom.reclik@gmail.com>
2022-01-24 16:31:19 +0100

    Added compiler optimization

--- hardhat.config.js
+++ hardhat.config.js
@@ -24,13 +24,31 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.6.12"
+        version: "0.6.12",
+        settings: {
+          optimizer: {
+              enabled: true,
+              runs: 1000,
+          },
+        },
       },
       {
-        version: "0.6.6"
+        version: "0.6.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
       },
       {
-        version: "0.7.3"
+        version: "0.7.3",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
       }
     ]
   }
